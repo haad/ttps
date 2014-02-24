b2d93186387ca01ef83e1fa25d9a40d7a8924c89
Adam Hamsik <adam.hamsik@innovatrics.com>
2014-02-24 23:17:56 +0100

    Initial import of ttps.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ttps::Application.config.secret_key_base = 'd97f59ecf67ea44092c043b6cca109b01652aa7cf4133a8b870f58f3482e786d13bfb2b0269cfb1b1be54d0f77e534d5d345825bb0a90a549dcf550478b12755'
